249134d0c50f75f7566b0650d4450ee97e1c8111
ignocide <ignocide@gmail.com>
2018-11-06 15:10:08 +0900

    add cors filter

--- gateway-service/src/main/java/ignocide/service/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/ignocide/service/gateway/GatewayApplication.java
@@ -1,16 +1,21 @@
 package ignocide.service.gateway;
 
 
+import ignocide.service.gateway.filter.pre.ForwardHeaderFilter;
 import ignocide.service.gateway.filter.pre.LoggingFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+
+@EnableZuulProxy
 @SpringBootApplication
 @EnableEurekaClient
-@EnableZuulProxy
 public class GatewayApplication {
 
     public static void main(String[] args) {
@@ -22,19 +27,23 @@ public class GatewayApplication {
         return new LoggingFilter();
     }
 
-//
-//    @Bean
-//    public CorsFilter corsFilter() {
-//        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        final CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.addAllowedOrigin("*");
-//        config.addAllowedHeader("*");
-//        config.addAllowedMethod("*");
-//        config.setMaxAge(3600L * 24 * 30); //30일에 한번만 OPTIONS 콜
-//        source.registerCorsConfiguration("/**", config);
-//        CorsFilter corsFilter = new CorsFilter(source);
-//        return corsFilter;
-//    }
+    @Bean
+    public ForwardHeaderFilter forwardHeaderFilter() {
+        return new ForwardHeaderFilter();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(3600L * 24 * 30); //30일에 한번만 OPTIONS 콜
+        source.registerCorsConfiguration("/**", config);
+        CorsFilter corsFilter = new CorsFilter(source);
+        return corsFilter;
+    }
 
 }
