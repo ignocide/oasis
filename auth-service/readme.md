099240eb2cbd9ec7c3453651a40de225a10a9f46
ignocide <ignocide@gmail.com>
2019-08-08 16:35:58 +0900

    auth-service 프로젝트 분리

--- auth-service/readme.md
+++
@@ -1,7 +0,0 @@
-# Auth Service
-
-### create server.jks
-
-```
-$ keytool -genkeypair -alias auth-service -keyalg RSA -keypass keypassword -keystore server.jks -storepass storepassword
-```
--- auth-service/src/main/java/ignocide/service/auth/repository/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ignocide.service.auth.repository.user;
-
-
-import ignocide.service.auth.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
