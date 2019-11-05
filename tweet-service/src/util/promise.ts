a633395d98cc68a3a70516c8c655e603782ca4e4
sungmin kim <ignocide@gmail.com>
2019-11-05 12:14:20 +0900

    tweet-service 프로젝트 분리

--- tweet-service/src/util/promise.ts
+++
@@ -1,7 +0,0 @@
-const series = async function (tasks: any) {
-    return tasks.reduce((chain: any, nextPromise: any) => {
-        return chain.then(nextPromise);
-    }, Promise.resolve());
-};
-
-export { series }
