380a765da57cb2eed20203ae6cd6a9f29941f88f
sungmin kim <ignocide@gmail.com>
2019-11-06 15:30:10 +0900

    문서화 typedoc 추가

--- woofer-service/src/config.ts
+++ woofer-service/src/config.ts
@@ -1,14 +1,14 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-const configFile = path.join(__dirname,'..','config.json')
+const configFile = path.join(__dirname, '..', 'config.json')
 
-let file = {};
-try{
+let file: any = {};
+try {
   file = fs.readFileSync(configFile, 'utf8');
   console.log(file)
   file = JSON.parse(file)
-} catch(e) {
+} catch (e) {
   throw new Error('config file is not exist, need to run yarn fetch:config')
 }
 
